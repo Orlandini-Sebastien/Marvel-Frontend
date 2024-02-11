ad1fec9359fc5ffde5441f6d86d88f7aa65598d4
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-02-11 19:18:08 +0100

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,44 +20,55 @@ const Header = ({
 
 	return (
 		<header className="bg-red-marvel text-white text-xl">
-			<div className="flex h-20 w-4/5 m-auto justify-between items-center ">
+			<div>
+				<div className="flex h-20 w-4/5 m-auto justify-between items-center ">
+					<div className="flex  h-full items-center">
+						<Link className="h-full flex items-center w-28" to={'/'}>
+							<img className="w-max" src={logo} alt="logo" />{' '}
+						</Link>
+					</div>
 
+					<div className="md:visible max-md:hidden flex justify-around w-1/2 h-full items-center">
+						<Link to={'/comics'}> Comics</Link>
+						<Link to={'/'}> Characters</Link>
+						<Link to={'/favoris'}>Favoris</Link>
+					</div>
 
-				<div className="flex  h-full items-center">
-					<Link className="h-full flex items-center w-28" to={'/'}>
-						<img className="w-max" src={logo} alt="logo" />{' '}
-					</Link>
+					<div className="flex justify-around  h-full items-center">
+						{token ? (
+							<button
+								onClick={() => {
+									Cookies.remove('userToken')
+									setToken('')
+									navigate('/')
+								}}
+							>
+								Deconnection
+							</button>
+						) : (
+							<div className="border-2 border-white border-solid rounded flex">
+								<button
+									className="mx-1 w-18"
+									onClick={() => setDisplayLogin(true)}
+								>
+									Log in
+								</button>
+								<div> | </div>
+								<button
+									className="mx-1 w-18"
+									onClick={() => setDisplaySignUp(true)}
+								>
+									Sign up
+								</button>
+							</div>
+						)}
+					</div>
 				</div>
-
-
-				<div className="flex justify-around w-1/2 h-full items-center">
+				<div className="max-md:visible md:hidden flex justify-around w-full h-full items-center">
 					<Link to={'/comics'}> Comics</Link>
 					<Link to={'/'}> Characters</Link>
 					<Link to={'/favoris'}>Favoris</Link>
 				</div>
-
-
-
-				<div className="flex justify-around  h-full items-center">
-					{token ? (
-						<button
-							onClick={() => {
-								Cookies.remove('userToken')
-								setToken('')
-								navigate('/')
-							}}
-						>
-							Deconnection
-						</button>
-					) : (
-						<>
-							<button className='mx-1' onClick={() => setDisplayLogin(true)}>Sign in</button>
-							<button className='mx-1' onClick={() => setDisplaySignUp(true)}>Sign up</button>
-						</>
-					)}
-				</div>
-
-
 			</div>
 		</header>
 	)
