49abf215092f267f6871cf715bc2556c7fa94dac
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-02-12 20:44:27 +0100

    first commit

---
+++ src/pages/Favoris.tsx
@@ -0,0 +1,281 @@
+import { useEffect, useState } from 'react'
+import axios, { AxiosResponse } from 'axios'
+import { useParams } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+import Cookies from 'js-cookie'
+import { AxiosError } from 'axios'
+import { useNavigate } from 'react-router-dom'
+import favorit from '../assets/favorits.png'
+
+type favorisCharacterType = {
+	_id: string
+	name: string
+	thumbnail: {
+		path: string
+		extension: string
+	}
+	description: string
+}
+type favoriComicType = {
+	_id: string
+	title: string
+	thumbnail: {
+		path: string
+		extension: string
+	}
+	description: string
+}
+
+type FavoType = {
+	token: string
+	setDisplayLogin: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const Favoris = ({ token, setDisplayLogin }: FavoType) => {
+	const { id } = useParams()
+	const location = useLocation()
+	console.log('location >>>>', location)
+	console.log(id)
+	const [isLoading, setIsLoading] = useState(true)
+	const [data, setData] = useState<Array<favorisCharacterType>>([])
+	const [data2, setData2] = useState<Array<favoriComicType>>([])
+	const navigate = useNavigate()
+	const descriptionToggleInitial: boolean[] = new Array(100).fill(false)
+	const [descriptionToggleCharacter, setDescriptionToggleCharacter] = useState<
+		Array<boolean>
+	>(descriptionToggleInitial)
+
+	const descriptionToggleInitial2: boolean[] = new Array(100).fill(false)
+	const [descriptionToggleComic, setDescriptionToggleComic] = useState<
+		Array<boolean>
+	>(descriptionToggleInitial2)
+
+	const handleDescriptionCharacter = (index: number) => {
+		const newValue = [...descriptionToggleCharacter]
+		if (newValue[index] === false) newValue[index] = true
+		else newValue[index] = false
+		setDescriptionToggleCharacter(newValue)
+	}
+
+	const handleDescriptionComic = (index: number) => {
+		const newValue = [...descriptionToggleComic]
+		if (newValue[index] === false) newValue[index] = true
+		else newValue[index] = false
+		setDescriptionToggleComic(newValue)
+	}
+
+	useEffect(() => {
+		console.log('dedans')
+
+		const fetchData = async () => {
+			try {
+				const depart = Cookies.get('FavoritCharacter')
+				if (depart) {
+					const cookie = JSON.parse(depart)
+					console.log('cookie>>>', cookie)
+
+					const data: never[] = []
+					for (let i = 0; i < cookie.length; i++) {
+						const response: AxiosResponse<never> = await axios.request({
+							headers: {
+								Authorization: `Bearer ${token}`,
+							},
+							method: 'GET',
+							url: `https://site--backend-marvel--cfvhczrj5zks.code.run/character/${cookie[i]}`,
+						})
+
+						data.push(response.data)
+
+						console.log('response', data)
+					}
+					setData(data)
+				} else {
+					console.log('pas de cookie')
+				}
+			} catch (e) {
+				const error = e as AxiosError
+
+				console.log('catch app>>>', error.response)
+				if (error.response?.status === 401) {
+					navigate('/')
+					setDisplayLogin(true)
+				}
+			}
+
+			try {
+				const depart = Cookies.get('FavoritComic')
+				if (depart) {
+					const cookie = JSON.parse(depart)
+					console.log('cookie>>>', cookie)
+
+					const data2: never[] = []
+					for (let i = 0; i < cookie.length; i++) {
+						const response: AxiosResponse<never> = await axios.request({
+							headers: {
+								Authorization: `Bearer ${token}`,
+							},
+							method: 'GET',
+							url: `https://site--backend-marvel--cfvhczrj5zks.code.run/comic/${cookie[i]}`,
+						})
+
+						data2.push(response.data)
+
+						console.log('response', data2)
+					}
+					setData2(data2)
+				} else {
+					console.log('pas de cookie')
+				}
+			} catch (e) {
+				const error = e as AxiosError
+
+				console.log('catch app>>>', error.response)
+				if (error.response?.status === 401) {
+					navigate('/')
+					setDisplayLogin(true)
+				}
+			}
+
+			setIsLoading(false)
+		}
+		fetchData()
+	}, [])
+
+	{
+		console.log('Dans favorisCaracter >>>>>', data)
+	}
+
+	return isLoading ? (
+		<div className="flex h-[85vh] w-screen justify-center items-center text-white text-4xl">
+			Loading ...
+		</div>
+	) : (
+		<section>
+				<img
+				className="w-full max-md:h-[50%]  fixed h-1/2   object-cover top-1/2 transform -translate-y-1/2 -z-10  "
+				src={favorit}
+				alt="oeil"
+			/>
+			{data.length > 0 ? (
+				<section className="m-auto w-5/6">
+					
+					<h1 className="flex justify-center  h-12 items-center font-bold text-black border border-solid rounded bg-white/70 shadow-white shadow-xl my-6">
+						FAVORITE CHARACTERS
+					</h1>
+					<div className="flex flex-wrap justify-center w-full ">
+						{data.map(
+							(favorisCaracter: favorisCharacterType, index: number) => {
+								return (
+									<div
+										className={
+											favorisCaracter.thumbnail.path &&
+											favorisCaracter.thumbnail.extension
+												? 'shadow-white cursor-alias  shadow-xl relative min-w-52  m-2 border-2 border-solid border-white rounded flex flex-col w-[18%] h-96 overflow-auto '
+												: 'hidden'
+										}
+										key={favorisCaracter._id}
+									>
+										<img
+											className="h-full object-cover"
+											src={`${favorisCaracter.thumbnail.path}.${favorisCaracter.thumbnail.extension}`}
+											alt={`image de ${favorisCaracter.name}`}
+										/>
+
+										<div
+											className={
+												descriptionToggleCharacter[index]
+													? 'absolute  font-bold  w-full bg-red-300 bg-opacity-60 flex rounded  flex-col '
+													: 'hidden'
+											}
+										>
+											<div className="flex mx-8 text-center  justify-center font-extrabold my-2 ">
+												{favorisCaracter.name}
+											</div>
+											<div>{favorisCaracter.description}</div>
+										</div>
+										<button
+											className={
+												favorisCaracter.description
+													? 'absolute left-1/2 transform -translate-x-1/2  z-10 bg-slate-100 bg-opacity-70  rounded bottom-4 border-2 border-red-400 font-bold '
+													: 'hidden'
+											}
+											onClick={() => handleDescriptionCharacter(index)}
+										>
+											DESCRIPTION
+										</button>
+									</div>
+								)
+							}
+						)}
+					</div>
+				</section>
+			) : (
+				<section className="text-white justify-center flex items-center font-extrabold flex-col">
+					<div>No favorite characters</div>
+					<div>😞</div>
+				</section>
+			)}
+			<section className='my-6'>
+				{data2.length > 0 ? (
+				<section className="m-auto w-5/6">
+					<h1 className="flex justify-center  h-12 items-center font-bold text-black border border-solid rounded bg-white/70 shadow-white shadow-xl my-6">
+						FAVORITE COMICS
+					</h1>
+					<div className="flex flex-wrap justify-center w-full ">
+						{data2.map((favorisComic: favoriComicType, index: number) => {
+							return (
+								<div
+									className={
+										favorisComic.thumbnail.path &&
+										favorisComic.thumbnail.extension
+											? 'shadow-white shadow-xl relative min-w-52  m-2 border-2 border-solid border-white rounded flex flex-col w-[18%] h-96 overflow-auto'
+											: 'hidden'
+									}
+									key={favorisComic._id}
+								>
+									<img
+										className="h-full object-cover"
+										src={`${favorisComic.thumbnail.path}.${favorisComic.thumbnail.extension}`}
+										alt={`image de ${favorisComic.title}`}
+									/>
+
+									<div
+										className={
+											descriptionToggleComic[index]
+												? 'absolute  font-bold  w-full bg-red-300 bg-opacity-60 flex rounded  flex-col '
+												: 'hidden'
+										}
+									>
+										<div className="flex mx-8 text-center  justify-center font-extrabold my-2 ">
+											{favorisComic.title}
+										</div>
+										<div>{favorisComic.description}</div>
+									</div>
+									<button
+										className={
+											favorisComic.description
+												? 'absolute left-1/2 transform -translate-x-1/2  z-10 bg-slate-100 bg-opacity-70  rounded bottom-4 border-2 border-red-400 font-bold '
+												: 'hidden'
+										}
+										onClick={() => handleDescriptionComic(index)}
+									>
+										DESCRIPTION
+									</button>
+								</div>
+							)
+						})}
+					</div>
+				</section>
+			) : (
+				<section className="text-white justify-center flex items-center font-extrabold flex-col">
+					<div>No favorite comics</div>
+					<div>😞</div>
+				</section>
+			)}
+			</section>
+			
+		</section>
+	)
+}
+
+export default Favoris
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,177 @@
+import '../App.css'
+
+import { ReactElement, useState } from 'react'
+
+import axios, { AxiosError } from 'axios'
+import Cookies from 'js-cookie'
+import { useLocation } from 'react-router'
+import { useNavigate } from 'react-router-dom'
+
+type SignUpProps = {
+	setToken: React.Dispatch<React.SetStateAction<string>>
+
+	setDisplayLogin: React.Dispatch<React.SetStateAction<boolean>>
+	setDisplaySignUp: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export default function SignUp({
+	setDisplayLogin,
+	setDisplaySignUp,
+	setToken,
+}: SignUpProps): ReactElement {
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [alert, setAlert] = useState('')
+	const [shake, setShake] = useState(false)
+
+	const location = useLocation()
+	const navigate = useNavigate()
+	console.log('location >>>', location.state)
+
+	const handleNameChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	): void => {
+		const value = event.target.value
+		setName(value)
+	}
+	const handleEmailChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	): void => {
+		const value = event.target.value
+		setEmail(value)
+	}
+	const handleP1Change = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		const value = event.target.value
+		setPassword(value)
+	}
+
+	const handleSubmit = async (event: React.MouseEvent<HTMLFormElement>) => {
+		event.preventDefault()
+		if (name === '') {
+			setAlert('Le nom est obligatoire !')
+		} else if (email === '') {
+			setAlert("L'email est obligatoire !")
+		} else if (password.length < 6) {
+			setAlert('7 charachères minimum !')
+			setShake(true)
+			setTimeout(() => {
+				setShake(false)
+			}, 1000)
+		} else {
+			setAlert('')
+
+			const cookieCharactersString = Cookies.get('FavoritCharacter')
+			const cookieComicsString = Cookies.get('FavoritComic')
+			
+			if (
+				name &&
+				email &&
+				password &&
+				cookieCharactersString &&
+				cookieComicsString
+			) {
+				try {
+					const { data } = await axios.post(
+						'https://site--backend-marvel--cfvhczrj5zks.code.run/userMarvel/signup',
+						{
+							username: name,
+							email: email,
+							password: password,
+							favorite_Characters: cookieCharactersString,
+							favorite_Comics: cookieComicsString,
+						}
+					)
+					console.log('response', data)
+					Cookies.set('userToken', data.token, { expires: 1, secure: true })
+					setToken(data.token)
+					setDisplaySignUp(false)
+					if (location.state.path)
+						navigate(location.state.path, {
+							state: {
+								price: location.state.price,
+								product_name: location.state.product_name,
+							},
+						})
+				} catch (e) {
+					const error = e as AxiosError
+
+					console.log('catch app>>>', error.response?.status)
+					if (error.response?.status === 400) {
+						setAlert("L'email est déjà enregistré")
+					}
+				}
+			}
+		}
+	}
+
+	return (
+		<>
+			<section className="flex flex-col w-11/12 h-full py-4 ">
+				<div className="w-full flex justify-end ">
+					<button onClick={() => setDisplaySignUp(false)} className="mx-4  ">
+						✖️
+					</button>
+				</div>
+				<div className="text-lg text-gray-600  flex justify-center">
+					Sign Up
+				</div>
+
+				<form
+					onSubmit={handleSubmit}
+					className="h-full flex flex-col justify-around "
+				>
+					<input
+						type="text"
+						placeholder="Name"
+						name="name"
+						value={name}
+						onChange={handleNameChange}
+						className=" bg-white   border-b-2 leading-8  flex w-full text-red-marvel"
+					/>
+
+					<input
+						type="email"
+						placeholder="Email"
+						name="email"
+						value={email}
+						onChange={handleEmailChange}
+						className=" bg-white leading-8 border-b-2 border-red-200 flex w-full text-red-marvel"
+					/>
+
+					<input
+						type="password"
+						placeholder="Password"
+						name="p1"
+						value={password}
+						onChange={handleP1Change}
+						className={` ${
+							alert === 'passwords are not the same' ||
+							alert === '7 charachers minimum !'
+								? 'borderRed'
+								: ''
+						}  ${
+							shake ? 'shake' : ''
+						}  bg-white  border-b-2 rounded w-full leading-8 `}
+					/>
+
+					<p className="text-red-500  sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
+						{alert}
+					</p>
+					<button className="bg-red-marvel mb-4 border-none rounded bg-blue-vinted text-white py-2 w-full">
+						Here we go !
+					</button>
+					<button
+						className="flex justify-center text-xs text-red-marvel w-full"
+						onClick={() => {
+							setDisplayLogin(true)
+							setDisplaySignUp(false)
+						}}
+					>
+						Already have an account? Log in
+					</button>
+				</form>
+			</section>
+		</>
+	)
+}
